544c9b68f433ec480d8bd4433b14a43c95b0a8f0
Oskar Nyberg <oskar@mullvad.net>
2023-11-22 13:36:41 +0100

    Add API access methods view

---
+++ gui/src/renderer/components/ApiAccessMethods.tsx
@@ -0,0 +1,52 @@
+import { messages } from '../../shared/gettext';
+import { useHistory } from '../lib/history';
+import InfoButton from './InfoButton';
+import { BackAction } from './KeyboardNavigation';
+import { Layout, SettingsContainer } from './Layout';
+import { NavigationBar, NavigationContainer, NavigationItems, TitleBarItem } from './NavigationBar';
+import SettingsHeader, { HeaderSubTitle, HeaderTitle } from './SettingsHeader';
+import { StyledContent, StyledNavigationScrollbars, StyledSettingsContent } from './SettingsStyles';
+
+export default function ApiAccessMethods() {
+  const history = useHistory();
+
+  return (
+    <BackAction action={history.pop}>
+      <Layout>
+        <SettingsContainer>
+          <NavigationContainer>
+            <NavigationBar>
+              <NavigationItems>
+                <TitleBarItem>
+                  {
+                    // TRANSLATORS: Title label in navigation bar
+                    messages.pgettext('navigation-bar', 'API access methods')
+                  }
+                </TitleBarItem>
+              </NavigationItems>
+            </NavigationBar>
+
+            <StyledNavigationScrollbars fillContainer>
+              <StyledContent>
+                <SettingsHeader>
+                  <HeaderTitle>
+                    {messages.pgettext('navigation-bar', 'API access methods')}
+                    <InfoButton message="TODO: Message goes here" />
+                  </HeaderTitle>
+                  <HeaderSubTitle>
+                    {messages.pgettext(
+                      'api-access-methods-view',
+                      'Manage and add custom methods to access the Mullvad API.',
+                    )}
+                  </HeaderSubTitle>
+                </SettingsHeader>
+
+                <StyledSettingsContent></StyledSettingsContent>
+              </StyledContent>
+            </StyledNavigationScrollbars>
+          </NavigationContainer>
+        </SettingsContainer>
+      </Layout>
+    </BackAction>
+  );
+}
