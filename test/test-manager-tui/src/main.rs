6c0091b2ae9d4d7672aa43a358bb474059d2edaf
Markus Pettersson <markus.pettersson@mullvad.net>
2023-11-24 15:04:09 +0100

    Add very primitive vim-bindings for navigating the list

--- test/test-manager-tui/src/main.rs
+++ test/test-manager-tui/src/main.rs
@@ -58,8 +58,8 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                 if key.kind == KeyEventKind::Press {
                     match key.code {
                         KeyCode::Char('q') => return Ok(()),
-                        KeyCode::Down => app.next(),
-                        KeyCode::Up => app.previous(),
+                        KeyCode::Down | KeyCode::Char('j') => app.next(),
+                        KeyCode::Up | KeyCode::Char('k') => app.previous(),
                         // Fold or Unfold the currently selected item
                         KeyCode::Tab => {
                             app.on_tab();
