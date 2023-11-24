4df64cae95a1e7fdb612f1071f3c8ec532515cfd
Markus Pettersson <markus.pettersson@mullvad.net>
2023-11-24 12:31:59 +0100

    Render the example ratatui table

--- test/test-manager-tui/src/main.rs
+++ test/test-manager-tui/src/main.rs
@@ -1,28 +1,42 @@
 use crossterm::{
-    event::{self, KeyCode, KeyEventKind},
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    ExecutableCommand,
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{
-    prelude::{CrosstermBackend, Stylize, Terminal},
-    widgets::Paragraph,
+    prelude::{Backend, Constraint, CrosstermBackend, Layout, Stylize, Terminal},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Cell, Paragraph, Row, Table, TableState},
+    Frame,
 };
-use std::io::{stdout, Result};
+use std::io::{self, stdout, Result};
 use test_manager_config::ConfigFile;
 
+struct App<'a> {
+    state: TableState,
+    items: Vec<Vec<&'a str>>,
+}
+
+#[allow(dead_code, unused)]
 fn main() -> Result<()> {
-    let config_path = dirs::config_dir()
-        .expect("Config directory not found. Can not load VM config")
-        .join("mullvad-test")
-        .join("config.json");
+    // load `test-manager` config
+    let mut config = {
+        let config_path = dirs::config_dir()
+            .expect("Config directory not found. Can not load VM config")
+            .join("mullvad-test")
+            .join("config.json");
+        ConfigFile::load_or_default(config_path).expect("Failed to load config")
+    };
 
-    let mut config = ConfigFile::load_or_default(config_path).expect("Failed to load config");
+    // Set up ratatui / terminal
+    let mut stdout = stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    stdout().execute(EnterAlternateScreen)?;
-    enable_raw_mode()?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-    terminal.clear()?;
-
+    // Run application
+    let app = App::new();
+    let res = run_app(&mut terminal, app);
     loop {
         terminal.draw(|frame| {
             let area = frame.size();
@@ -42,7 +56,108 @@ fn main() -> Result<()> {
         }
     }
 
-    stdout().execute(LeaveAlternateScreen)?;
+    // Restore terminal
     disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
     Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Down => app.next(),
+                    KeyCode::Up => app.previous(),
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn ui(f: &mut Frame, app: &mut App) {
+    let rects = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .split(f.size());
+
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let normal_style = Style::default().bg(Color::Blue);
+    let header_cells = ["Name", "VM-type", "OS"]
+        .iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+    let header = Row::new(header_cells)
+        .style(normal_style)
+        .height(1)
+        .bottom_margin(1);
+    let rows = app.items.iter().map(|item| {
+        let height = item
+            .iter()
+            .map(|content| content.chars().filter(|c| *c == '\n').count())
+            .max()
+            .unwrap_or(0)
+            + 1;
+        let cells = item.iter().map(|c| Cell::from(*c));
+        Row::new(cells).height(height as u16).bottom_margin(1)
+    });
+    let t = Table::new(rows)
+        .header(header)
+        .block(Block::default().borders(Borders::ALL).title("Table"))
+        .highlight_style(selected_style)
+        .highlight_symbol(">> ")
+        .widths(&[
+            Constraint::Percentage(50),
+            Constraint::Max(30),
+            Constraint::Min(10),
+        ]);
+    f.render_stateful_widget(t, rects[0], &mut app.state);
+}
+
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            state: TableState::default(),
+            items: vec![
+                vec!["Debian 12", "QEMU", "Linux"],
+                vec!["Ubuntu 22.04", "QEMU", "Linux"],
+                vec!["Windows 10", "QEMU", "Windows"],
+            ],
+        }
+    }
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
 }
