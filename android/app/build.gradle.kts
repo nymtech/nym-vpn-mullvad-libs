6dfc204e14c6e8fdf962dd7308e4ce6621a4aee9
Albin <albin@mullvad.net>
2023-11-27 15:55:10 +0100

    Set test build version code to 23080023

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "net.mullvad.mullvadvpn"
         minSdk = Versions.Android.minSdkVersion
         targetSdk = Versions.Android.targetSdkVersion
-        versionCode = generateVersionCode(localProperties)
+        versionCode = 23080023
         versionName = generateVersionName(localProperties)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
