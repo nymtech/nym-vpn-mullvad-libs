3e9563fd4cbd8d1bb874112d42b97c076d45fc7c
Markus Pettersson <markus.pettersson@mullvad.net>
2024-02-06 16:05:45 +0100

    WIP Add Shadowsocks access method test

--- test/test-manager/src/tests/access_methods.rs
+++ test/test-manager/src/tests/access_methods.rs
@@ -21,17 +21,49 @@ use test_rpc::ServiceClient;
 pub async fn test_custom_access_methods(
     _: TestContext,
     _rpc: ServiceClient,
-    _mullvad_client: MullvadProxyClient,
+    mullvad_client: MullvadProxyClient,
 ) -> Result<(), Error> {
     log::info!("Testing Shadowsocks access method");
-    test_shadowsocks().await?;
+    test_shadowsocks(mullvad_client.clone()).await?;
     log::info!("Testing SOCKS5 (Remote) access method");
     test_socks_remote().await?;
     Ok(())
 }
 
-async fn test_shadowsocks() -> Result<(), Error> {
-    panic!("Testing Shadowsocks access method has not been fully implemented yet!")
+async fn test_shadowsocks(mut mullvad_client: MullvadProxyClient) -> Result<(), Error> {
+    use mullvad_types::relay_list::RelayEndpointData;
+    use talpid_types::net::proxy::CustomProxy;
+    // Set up all the parameters needed to create a custom Shadowsocks access method.
+    //
+    // Since Mullvad host's Shadowsocks relays on their bridge servers, we can
+    // simply select a bridge server to derive all the needed parameters.
+    // mullvad_client
+    let relay_list = mullvad_client.get_relay_locations().await.unwrap();
+    let bridge = relay_list
+        .relays()
+        .filter(|relay| matches!(relay.endpoint_data, RelayEndpointData::Bridge))
+        .nth(0)
+        .expect("`test_shadowsocks` needs at least one shadowsocks relay to execute. Found non in relay list.");
+
+    let access_method: CustomProxy = relay_list
+        .bridge
+        .shadowsocks
+        .first()
+        .map(|shadowsocks| {
+            shadowsocks.to_proxy_settings(bridge.ipv4_addr_in.into())
+        })
+        .expect("`test_shadowsocks` needs at least one shadowsocks relay to execute. Found non in relay list.");
+
+    let successful = mullvad_client
+        .test_custom_api_access_method(access_method.clone().into())
+        .await?;
+
+    assert!(
+        successful,
+        "Failed while testing access method - {access_method:?}"
+    );
+
+    Ok(())
 }
 
 #[allow(clippy::unused_async)]
